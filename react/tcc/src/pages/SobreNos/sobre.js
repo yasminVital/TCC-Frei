d154f21f2ae71911a7a64f6ed68b418960d080be
Bruno <bruno.souza.c2014@gmail.com>
2021-09-26 18:59:59 -0300

    João seu lindo

---
+++ react/tcc/src/pages/SobreNos/sobre.js
@@ -0,0 +1,165 @@
+import { COnteinersobrenos } from './styled'
+
+export default function sobrenos () {
+
+    return(
+        <COnteinersobrenos>
+
+<div className= 'cabecalho'>
+            <div className="filho">
+                <div className= 'cabe'>
+                HOME
+                </div>
+                <div className= 'cabe'>
+                SOBRE NÓS
+                </div>
+                <div className= 'cabe'>
+                MENU
+                </div>
+            </div>
+            <div className = 'filho1'>
+            <div className= 'barra'>
+            <svg width="4" height="50" viewBox="0 0 8 84" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="84" fill="#444444"/></svg>
+            </div>
+            <div className= 'Lup-img'>
+            <img src="./assets/imagens/lupa1.svg" height="25" alt="" />
+            </div>
+
+            <div className= 'barra'>
+            <svg width="4" height="50" viewBox="0 0 8 84" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="84" fill="#444444"/></svg>
+            </div>
+
+            <div className= 'cadastro'>
+            <img src="./assets/imagens/logo de user.png" height="25" alt="" />
+            </div>
+            <div className= 'Cabecho'>
+                Entre / Cadastro
+            </div>
+            <div className= 'barra'>
+            <svg width="4" height="50" viewBox="0 0 8 84" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="4" height="84" fill="#444444"/></svg>
+            </div>
+            <div className= 'carrinho'> 
+            <img src="./assets/imagens/mbricartadd_99553.svg" alt="" />
+            </div>
+            </div>
+        </div>
+        
+        <div className= 'Faixa-sobrenos'>
+        
+            <div className= 'fundo-fsn'>
+                <div className= 'titulo-fns'> Nossa Historia </div>
+                <div className= 'texto-fns'> <span> Projeto Auto-Sustentável: </span>   Uma vez completado integralmente o projeto, além de formar 1.500 jovens por
+                ano em mais de 20 profissões diferentes, se tornará auto-sustentável, na medida em que algumas das
+                profissões serão utilizadas como alavancagem, no sentido de gerar recursos para Manutenção e Reparos
+                do Prédio e dos Equipamentos, bem como para pagamento da Folha de Funcionários e Professores, além
+                da compra de pequenos equipamentos.    
+                </div>
+                <div className= 'texto-fns2'> As áreas de panificação , gráfica, fotolito, publicidade, rádio e Tv, serão as mais utilizadas em <span> Serviços
+                para Terceiros. </span> Já estamos funcionando dentro desta estratégia com a padaria, e iniciando com a gráfica
+                (incompleta), uma vez que dispomos apenas de uma pequena Off-Set, insuficiente para enfrentar a dura
+                concorrência do mercado. Estamos tendo a oportunidade de arcar com todas as despesas de
+                manutenção e folha, com grandes sacrifícios na medida em que não dispomos de todos os recursos
+                necessários, contudo com a complementação do projeto, teremos assegurado a sua perenidade.
+            </div>
+            </div>
+
+            <div className = 'montagem-padaria'>
+
+                <div className = 'titulo'>
+                Tradicional da Padaria FREI
+                </div>
+
+                <div className = 'img-pt1'>
+                    <div className= 'img-pr1'>
+                        <img src="./assets/imagens/Pao-doce-coco-creme-Baixa-5 3.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr1'>
+                        <img src="./assets/imagens/sonho.png" height="158" width="258"  alt="" />
+                    </div>
+                    <div className= 'img-pr1'>
+                        <img src="./assets/imagens/Bolo de Fubá.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr1'>
+                        <img src="./assets/imagens/bolo com cobertura.png" height="158" width="258" alt="" />
+                    </div>
+                </div>
+
+                <div className = 'img-pt2'>
+                    <div className= 'img-pr2'>
+                        <img src="./assets/imagens/unnamed 3.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr2'>
+                        <img src="./assets/imagens/Bolo de pote.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr2'>
+                        <img src="./assets/imagens/Brigadeiro-Cremoso-Simples-no-Pote-380x254 3.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr2'>
+                        <img src="./assets/imagens/cafe.png" height="158" width="258" alt="" />
+                    </div>
+                </div>
+
+                <div className = 'img-pt3'>
+                    <div className= 'img-pr3'>
+                        <img src="./assets/imagens/29327-sucos-funcionais-extraem-o-melhor-das-fr-article_block_media-2 2 (2).png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr3'>
+                        <img src="./assets/imagens/receitas-de-misto-quente 2.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr3'>
+                        <img src="./assets/imagens/queijo quente-380x380 2.png" height="158" width="258" alt="" />
+                    </div>
+                    <div className= 'img-pr3'>
+                        <img src="./assets/imagens/1582203069205 2.png" height="158" width="258" alt="" />
+                    </div>
+                </div>
+
+            </div>
+
+            <div className = 'rodape'>
+                
+                <div className = 'part-roda1'> 
+                    <div className='info-01'> 
+                    Informações de contato
+                    </div>
+                    <div className='info-02'> 
+                    Av. Coronel Octaviano de Freitas Costa, 463 - Socorro. São Paulo
+                    </div>
+                    <div className='info-03'> 
+                    Telefone: (11) 5687-8876 | 96398-6252
+                    </div>
+                    <div className='info-04'> 
+                    contato@testefigma.com
+                    </div>
+                </div>
+
+                <div className= 'part-roda2'>
+                    <div className = 'info-05'> 
+                    Horário De  Funcionamento 
+                    </div>
+                    <div className = 'info-06'> 
+                    De Segunda à Sexta-Feira
+                        08:00 às 16:30
+                    </div>
+                </div>
+
+                <div className= ' part-roda3'>
+                    <div className= 'img'>
+                       <img src="./assets/imagens/logo 13 (1).png" alt= ""/>
+                    </div>
+                    </div>
+                    </div>
+                <div className= 'final'>
+                    <div className= 'exp'>
+                    Todos os direitos reservados | STI ©
+                </div>
+                </div>
+
+
+
+
+         </div>
+        
+        </COnteinersobrenos>
+    )
+}
