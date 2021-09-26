c49738d8079fe26b9847a14f187af691e9f68118
Joao <joaopedrosilvadossantos2003@gmail.com>
2021-09-26 15:28:18 -0300

    g

--- react/tcc/src/index.js
+++ react/tcc/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './routes';
+
+import Routes from './Routes';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes/>
   </React.StrictMode>,
   document.getElementById('root')
 );
