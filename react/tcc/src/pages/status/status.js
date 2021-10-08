fc8442823cb2ce3f8f9ce03d43e464fb6c37293e
Bruninho <bruno.souza.c2014@gmail.com>
2021-10-08 09:36:23 -0300

    BOM DIA

--- react/tcc/src/pages/status/status.js
+++ react/tcc/src/pages/status/status.js
@@ -1,5 +1,7 @@
 import {Conteinerstatus} from './styled'
 
+import { Link } from "react-router-dom"
+
 export default function status () {
 
     return (
@@ -79,7 +81,7 @@ export default function status () {
                 </div>
                 </div>
                 <div className = "volta">
-                    <img src= "./assets/imagens/icon-voltar-removebg-preview 2.png" alt= ""/>
+                    <Link to="./revisao"> <img src= "./assets/imagens/icon-voltar-removebg-preview 2.png" alt= ""/>  </Link>
                 </div>
                 <div className= "tet1">
                     Previsão de Entrega
@@ -174,7 +176,44 @@ export default function status () {
                             </div> 
                             
                         </div>
+                        <div className='rodape'>
 
+<div className='part-roda1'>
+    <div className='info-01'>
+        Informações de contato
+    </div>
+    <div className='info-02'>
+        Av. Coronel Octaviano de Freitas Costa, 463 - Socorro. São Paulo
+    </div>
+    <div className='info-03'>
+        Telefone: (11) 5687-8876 | 96398-6252
+    </div>
+    <div className='info-04'>
+        contato@testefigma.com
+    </div>
+</div>
+
+<div className='part-roda2'>
+    <div className='info-05'>
+        Horário De  Funcionamento
+    </div>
+    <div className='info-06'>
+        De Segunda à Sexta-Feira <br/> <br/>
+          08:00 às 16:30 
+    </div>
+</div>
+
+<div className=' part-roda3'>
+    <div className='img'>
+        <img src="./assets/imagens/logo 13 (1).png" alt="" width="80%"/>
+    </div>
+</div>
+</div>
+<div className='final3'>
+<div className='exp'>
+    Todos os direitos reservados | STI ©
+</div>
+</div>
                     
                     
 
