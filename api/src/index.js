62b7a3c2e75c2a48e4483c10b1794e2b7f792d3e
João Pedro <joaopedrosilvadossantos2003@gmail.com>
2021-10-25 15:29:56 -0300

    o

--- api/src/index.js
+++ api/src/index.js
@@ -7,20 +7,58 @@ const app = new express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/login', async (req, resp) => {
-    const email = req.body.login;
-    const senha = req.body.senha;
 
-    let resp = await db.infoa_sti_cliente.findOne({
+app.post('/login', async (req, resp) => {
+
+
+    let login = req.body;
+
+    let p = await db.infoa_sti_cliente.findOne({
         where: {
-            ds_email: email,
-            ds_senha: senha
+            ds_email: login.email,
+            ds_senha: login.senha
         },
         raw: true
     })
 
-    if(resp == null)
+    if(p == null)
     return resp.send({erro: 'tudo errado'});
 
     resp.sendStatus(200);
 });
+
+
+app.post('/cadastrar', async (req, resp) => {
+    let x = req.body;
+     
+    try {
+    const UsuarioCriado= await db.infoa_sti_cliente.create({
+        id_endereco: EnderecoCriado.id_endereco,
+        nm_nome: x.nome,
+        nm_sobrenome: x.sobrenome,
+        ds_sexo: x.sexo,
+        ds_cpf: x.cpf,
+        dt_nascimento: x.nascimento,
+        ds_email: x.email,
+        ds_senha: x.senha
+    })
+
+
+    const EnderecoCriado = await db.infoa_sti_endereco.create({
+        ds_cep: x.cep,
+        ds_endereco: x.endereco,
+        nr_endereco: x.numero,
+        ds_complemento: x.complemento,
+        ds_cidade: x.cidade
+    })
+
+    resp.send(200);
+
+} catch (error) {
+    resp.send( error.toString() )
+}
+})
+
+
+app.listen(process.env.PORT,
+            console.log(`Servidor na Porta ${process.env.PORT}`));
