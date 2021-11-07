29309caff18f9597953d92d0678bb4955784b94c
yasmin <yasminvitaldesousa@gmail.com>
2021-11-07 13:36:40 -0300

    foii

--- react/tcc/src/pages/Login/esqueceuSenha/novasenha/novasenha.js
+++ react/tcc/src/pages/Login/esqueceuSenha/novasenha/novasenha.js
@@ -8,14 +8,13 @@ export default function novasenha() {
         <Conteinernovasenha>
             <div className = "Conteinernovasenha">
                 <div className  = "Login">
+                 <div className  = "logar"> <p>Enviamos um código de verificação para seu email. Digite o código abaixo:</p> </div>
 
-                
-               <div className  = "logar"> Enviamos um código de verificação para seu email. Digite o código abaixo: </div>
-               <div className = "inputs">
-               <div className  = "email"> <input type = "email"  placeholder= "Digite o código"/>  </div>
-               <div className  = "senhanova"> <input type = "password"  placeholder= "Digite sua nova senha"/>  </div>
-               <div className  = "confsenha"> <input type = "password" placeholder = "Confirme sua senha"/></div>
-                </div>
+                 <div className = "inputs">
+                   <div className  = "email"> <input type = "email"  placeholder= "Digite o código"/></div>
+                   <div className  = "senhanova"> <input type = "password"  placeholder= "Digite sua nova senha"/></div>
+                   <div className  = "confsenha"> <input type = "password" placeholder = "Confirme sua senha"/></div>
+                 </div>
 
 
                  <div className = "botão"> <Link  to = "/"> <button> Alterar </button>  </Link> </div>
